ccb8b4df3210aae18a74015b7c98cf9f9182649f
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-01 18:01:21 +0200

    Fix build and transpile package

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -3,6 +3,7 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@v1/supabase"],
   experimental: {
     instrumentationHook: process.env.NODE_ENV === "production",
   },
