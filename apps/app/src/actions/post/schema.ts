c1cb31a92847c3116714d35f3b4d82df2b1964df
Pontus Abrahamsson <pontus@lostisland.co>
2024-09-07 10:12:10 +0200

    Create services

--- apps/app/src/actions/post/schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const shareLinkSchema = z.object({
-  postId: z.string(),
-  baseUrl: z.string(),
-});
--- apps/app/src/actions/safe-action.ts
+++ apps/app/src/actions/safe-action.ts
@@ -1,6 +1,3 @@
-import * as Sentry from "@sentry/nextjs";
-import { setupAnalytics } from "@v1/analytics/server";
-import { ratelimit } from "@v1/kv/ratelimit";
 import { logger } from "@v1/logger";
 import { getUser } from "@v1/supabase/queries";
 import { createClient } from "@v1/supabase/server";
@@ -8,8 +5,6 @@ import {
   DEFAULT_SERVER_ERROR_MESSAGE,
   createSafeActionClient,
 } from "next-safe-action";
-import { headers } from "next/headers";
-import { z } from "zod";
 
 const handleServerError = (e: Error) => {
   console.error("Action error:", e.message);
@@ -27,17 +22,6 @@ export const actionClient = createSafeActionClient({
 
 export const actionClientWithMeta = createSafeActionClient({
   handleServerError,
-  defineMetadataSchema() {
-    return z.object({
-      name: z.string(),
-      track: z
-        .object({
-          event: z.string(),
-          channel: z.string(),
-        })
-        .optional(),
-    });
-  },
 });
 
 export const authActionClient = actionClientWithMeta
@@ -54,26 +38,8 @@ export const authActionClient = actionClientWithMeta
 
     return result;
   })
-  .use(async ({ next, metadata }) => {
-    const ip = headers().get("x-forwarded-for");
 
-    const { success, remaining } = await ratelimit.limit(
-      `${ip}-${metadata.name}`,
-    );
-
-    if (!success) {
-      throw new Error("Too many requests");
-    }
-
-    return next({
-      ctx: {
-        ratelimit: {
-          remaining,
-        },
-      },
-    });
-  })
-  .use(async ({ next, metadata }) => {
+  .use(async ({ next }) => {
     const {
       data: { user },
     } = await getUser();
@@ -83,22 +49,10 @@ export const authActionClient = actionClientWithMeta
       throw new Error("Unauthorized");
     }
 
-    if (metadata) {
-      const analytics = await setupAnalytics({
-        userId: user.id,
-      });
-
-      if (metadata.track) {
-        analytics.track(metadata.track);
-      }
-    }
-
-    return Sentry.withServerActionInstrumentation(metadata.name, async () => {
-      return next({
-        ctx: {
-          supabase,
-          user,
-        },
-      });
+    return next({
+      ctx: {
+        supabase,
+        user,
+      },
     });
   });
