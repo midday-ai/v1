92f128f75ad44466f017a7920c56ebdc354b50ed
OopsOverflow <houssemmahmoud18@gmail.com>
2024-10-06 02:33:41 +0200

    build(apps): use next lint instead of biome

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
   experimental: {
     instrumentationHook: process.env.NODE_ENV === "production",
   },
+  reactStrictMode: true,
 };
 
 export default withSentryConfig(nextConfig, {
